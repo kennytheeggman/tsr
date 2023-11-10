d388b8aed45eb0962b2e84e947309b92568c19bd
kennytheeggman <kenny.kh.huang@gmail.com>
2023-11-10 15:39:20 -0500

    add code for disparity mapping

---
+++ disparity.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+
+if __name__ == "__main__":
+    # init video feeds
+    vid1 = cv2.VideoCapture("videos/seq 20 vid 1.mp4")
+    vid2 = cv2.VideoCapture("videos/seq 20 vid 2.mp4")
+
+    # qol parameters
+    initial_frame_offset = 70
+    # feed matching parameters
+    resized = (400, 800)
+    frame_diff = 12  # 8; 12
+    mim_crop = 9, 0  # 8, 37; 5, 35
+    size = (resized[0] - mim_crop[1], resized[1] - mim_crop[0])
+    
+    vid1.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset + frame_diff)
+    vid2.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset)
+
+    if not (vid1.isOpened() and vid2.isOpened()):
+        print("videos not opened")
+        raise RuntimeError
+    
+    ret1, frame1 = vid1.read()
+    ret2, frame2 = vid2.read()
+    if not (ret1 and ret2):
+        print("frames not available")
+        raise RuntimeError
+
+    frame1 = cv2.resize(frame1, resized)[mim_crop[0]:resized[1], mim_crop[1]:resized[0]]
+    frame2 = cv2.resize(frame2, resized)[0:size[1], 0:size[0]]
+
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_RGB2GRAY)
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
+
+    stereo = cv2.StereoBM_create(numDisparities=64, blockSize=15)
+    stereo.setTextureThreshold(1000)
+    disparity = stereo.compute(frame1, frame2)
+
+    while not (cv2.waitKey(1) & 0xFF == ord('q')):
+        cv2.imshow("frame1", frame1)
+        cv2.imshow("frame2", frame2)
+        cv2.imshow("disparity", disparity*256)
+
+    vid1.release()
+    vid2.release()
+    
+    cv2.destroyAllWindows()
+
--- main.py
+++ main.py
@@ -1,37 +1,100 @@
 import cv2
+import numpy as np
+from numpy.linalg import inv
 
-cap1 = cv2.VideoCapture("BasketBall_Input_14.avi")
-cap2 = cv2.VideoCapture("BasketBall_Input_30.avi")
-cap3 = cv2.VideoCapture("BasketBall_Input_5.avi")
-# Check if camera opened successfully
 
-counter = 0
-frame_num = 0
+# dot product of pixels of a frame and a matrix
+def frame_dot(frame, matrix):
+    # error assertion
+    if not (frame.shape[2] == matrix.shape[0] == matrix.shape[1]):
+        raise ValueError("depth of frame did not match matrix size, or matrix was not square")
+    # deep copy frame and cast to float
+    dc_frame = cv2.resize(frame, (frame.shape[1], frame.shape[0])).astype(np.float64)
+    shape = matrix.shape[0]
 
-# Read until video is completed
-while cap1.isOpened():
-    counter+=1
-    counter%=3
-    # Capture frame-by-frame
-    feed = cap1 if counter == 1 else cap2 if counter == 2 else cap3
-    if counter == 0:
-        frame_num += 25
-    ret, frame = feed.read()
-    feed.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+    # sum-product
+    for i in range(shape):
+        for j in range(shape):
+            dc_frame[:, :, i] += dc_frame[:, :, i] * matrix[i, j]
 
-    if not ret:
-        break
-    # Display the resulting frame
-    cv2.imshow('Frame', frame)
+    # recast frame
+    dc_frame = cv2.max(dc_frame, 0).astype(np.uint8)
+    return dc_frame
 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(100) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the video capture object
-cap1.release()
-cap2.release()
-cap3.release()
+# contribution function of a timing point between 0 and 1
+def c_func(t):
+    return t
 
-# Closes all the frames
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    # init video feeds
+    vid1 = cv2.VideoCapture("videos/seq 20 vid 1.mp4")
+    vid2 = cv2.VideoCapture("videos/seq 20 vid 2.mp4")
+
+    # qol parameters
+    initial_frame_offset = 60
+    # feed matching parameters
+    resized = (400, 800)
+    frame_diff = 12  # 8; 12
+    mim_crop = 5, 35  # 8, 37; 5, 35
+    size = (resized[0] - mim_crop[1], resized[1] - mim_crop[0])
+    # superresolution parameters
+    q = 0.8
+    a = np.array([[1, 1-c_func(1-q/2)], [c_func(q/2), 1]])
+    a_inv = inv(a)
+    print(a_inv)
+
+    # video writer
+    writer = cv2.VideoWriter("test1.mp4", -1, 60, size, True)
+
+    # set initial frames
+    vid1.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset + frame_diff)
+    vid2.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset)
+
+    # while videos are open
+    while vid1.isOpened() and vid2.isOpened():
+        ret1, frame1 = vid1.read()
+        ret2, frame2 = vid2.read()
+
+        # end of video
+        if not ret1 or not ret2:
+            break
+
+        # resize with matching params
+        frame1 = cv2.resize(frame1, resized)[mim_crop[0]:resized[1], mim_crop[1]:resized[0]]
+        frame2 = cv2.resize(frame2, resized)[0:size[1], 0:size[0]]
+
+        # linalg superresolution
+        calc_r = frame_dot(np.dstack((frame1[:, :, 0], frame2[:, :, 0])), a_inv)
+        calc_g = frame_dot(np.dstack((frame1[:, :, 1], frame2[:, :, 1])), a_inv)
+        calc_b = frame_dot(np.dstack((frame1[:, :, 2], frame2[:, :, 2])), a_inv)
+
+        calc_1 = np.dstack((calc_r[:, :, 0], calc_g[:, :, 0], calc_b[:, :, 0]))
+        calc_2 = np.dstack((calc_r[:, :, 1], calc_g[:, :, 1], calc_b[:, :, 1]))
+
+        # show frames
+        cv2.imshow("blend", frame1)
+        cv2.imshow("calc", calc_2)
+        if cv2.waitKey(500) & 0xFF == ord('q'):
+            break
+        cv2.imshow("calc", calc_1)
+
+        # blending for calibration
+        # blend = np.array(dimensions + (3,), np.uint8)
+        # blend = (frame1 * 0.5).astype(np.uint8) + (frame2 * 0.5).astype(np.uint8)
+
+        # write to video
+        writer.write(calc_2)
+        writer.write(calc_1)
+
+        # keyboard exit condition
+        if cv2.waitKey(500) & 0xFF == ord('q'):
+            break
+
+    # release handles
+    vid1.release()
+    vid2.release()
+    writer.release()
+
+    cv2.destroyAllWindows()

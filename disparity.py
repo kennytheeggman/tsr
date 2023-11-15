d8d358cf15ea8ecc618fa32b99996e2044c3b71c
kennytheeggman <kenny.kh.huang@gmail.com>
2023-11-15 09:46:12 -0500

    updated for video instead of single frame

--- disparity.py
+++ disparity.py
@@ -18,33 +18,35 @@ if __name__ == "__main__":
     vid1.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset + frame_diff)
     vid2.set(cv2.CAP_PROP_POS_FRAMES, initial_frame_offset)
 
-    if not (vid1.isOpened() and vid2.isOpened()):
-        print("videos not opened")
-        raise RuntimeError
+    while vid1.isOpened() and vid2.isOpened():
     
-    ret1, frame1 = vid1.read()
-    ret2, frame2 = vid2.read()
-    if not (ret1 and ret2):
-        print("frames not available")
-        raise RuntimeError
+        ret1, frame1 = vid1.read()
+        ret2, frame2 = vid2.read()
+        if not (ret1 and ret2):
+            print("frames not available")
+            raise RuntimeError
 
-    frame1 = cv2.resize(frame1, resized)[mim_crop[0]:resized[1], mim_crop[1]:resized[0]]
-    frame2 = cv2.resize(frame2, resized)[0:size[1], 0:size[0]]
+        frame1 = cv2.resize(frame1, resized)[mim_crop[0]:resized[1], mim_crop[1]:resized[0]]
+        frame2 = cv2.resize(frame2, resized)[0:size[1], 0:size[0]]
 
-    frame1 = cv2.cvtColor(frame1, cv2.COLOR_RGB2GRAY)
-    frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_RGB2GRAY)
+        frame2 = cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
 
-    stereo = cv2.StereoBM_create(numDisparities=128, blockSize=21)
-    stereo.setMinDisparity(4)
-    stereo.setSpeckleRange(16)
-    stereo.setSpeckleWindowSize(45)
-    stereo.setTextureThreshold(1000)
-    disparity = stereo.compute(frame1, frame2)
+        stereo = cv2.StereoBM_create(numDisparities=48, blockSize=5)
+        stereo.setMinDisparity(4)
+        stereo.setSpeckleRange(16)
+        stereo.setSpeckleWindowSize(45)
+        stereo.setTextureThreshold(100)
+        disparity1 = stereo.compute(frame1, frame2)
+        # disparity2 = stereo.compute(frame2, frame1)
 
-    while not (cv2.waitKey(1) & 0xFF == ord('q')):
         cv2.imshow("frame1", frame1)
         cv2.imshow("frame2", frame2)
-        cv2.imshow("disparity", disparity*256)
+        cv2.imshow("disparity1", disparity1*256)
+        # cv2.imshow("disparity2", -disparity2*256)
+
+        if cv2.waitKey(250) & 0xFF == ord('q'):
+            break
 
     vid1.release()
     vid2.release()
